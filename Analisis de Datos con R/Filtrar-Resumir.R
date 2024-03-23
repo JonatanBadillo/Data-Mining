f9e2bb3c1dfbb44e24570c786ef9bb2fc32c0d16
Jonatan Badillo <jonatanbadillo.19@gmail.com>
2024-03-23 14:17:34 -0600

    Elimina datos innecesarios de una manera eficiente

--- Analisis de Datos con R/Filtrar-Resumir.R
+++ Analisis de Datos con R/Filtrar-Resumir.R
@@ -12,3 +12,40 @@ subset(mtcars,am == 1 & vs == 1)
   # Eliminar columnas(solo seleccionar las columnas especificadaws)
 subset(mtcars,am == 1 & vs == 1, select = hp:wt)
 subset(mtcars,am == 1 & vs == 1, select = wt)
+
+
+# ------------------------------------------------------------------------------
+# Elimina datos innecesarios de una manera eficiente
+library(hflights)
+system.time(sqldf("SELECT * FROM hflights WHERE Dest == 'BNA'",row.names = TRUE))
+#   user  system elapsed 
+#   0.737   0.019   1.061
+
+system.time(subset(hflights,Dest== 'BNA'))
+#   user  system elapsed 
+#   0.007   0.002   0.010 
+
+
+library(dplyr)
+system.time(filter(hflights, Dest == 'BNA'))
+#  user  system elapsed 
+#  0.010   0.001   0.012 
+
+# podemos extender esta solución eliminando algunas columnas del conjunto de datos como lo hicimos antes con el subconjunto
+str(select(filter(hflights,Dest == 'BNA'),DepTime:ArrTime))
+
+
+# los nombres de las filas no se conservan en dplyr, 
+# por lo que si los necesitas, vale la pena copiar los nombres en variables
+
+mtcars$rownames <- rownames(mtcars)
+select(filter(mtcars, hp>300),c(rownames,hp))
+
+
+
+
+
+
+
+
+explícitas antes de pasarlas a dplyr o
