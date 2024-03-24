1d5b8166701db55e300b7254d8b28bf4802d22b1
Jonatan Badillo <jonatanbadillo.19@gmail.com>
2024-03-23 18:06:43 -0600

    histograma

--- Analisis de Datos con R/Visualizacion-Datos.R
+++ Analisis de Datos con R/Visualizacion-Datos.R
@@ -18,6 +18,12 @@ data
 
 barplot(data, xlab="x-axis", ylab="y-axis", main="bar chart 1", col=c("grey", "blue", "yellow"))
 
-
 # Un gráfico de barras agrupadas
 barplot(data, xlab="x-axis", ylab="y-axis", main="bar chart 1", col=c("grey", "blue", "yellow"),beside=TRUE)
+
+
+
+# Histogramas
+set.seed(123)
+data1 <- rnorm(100, mean=5, sd=3)
+hist(data1, main="histogram", xlab="x-axis", col="green",border="blue", breaks=10)
