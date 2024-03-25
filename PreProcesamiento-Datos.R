c1198ab8df2df85eb15a93cfc9a5a3498b17ed1f
Jonatan Badillo <jonatanbadillo.19@gmail.com>
2024-03-25 14:04:05 -0600

    Codificación de datos categóricos

--- PreProcesamiento-Datos.R
+++ PreProcesamiento-Datos.R
@@ -14,6 +14,7 @@ View(Dataset)
 #   • El segundo parámetro es el valor que ingresamos si la condición es verdadera.
 #   • El tercer parámetro es la acción que tomamos si la condición es falsa.
 
+
 Dataset$Age=
   # is.na(Dataset$Age). Esto nos dirá si falta o no un valor en Dataset$Age.
   ifelse(is.na(Dataset$Age),
@@ -25,8 +26,36 @@ Dataset$Age=
          # La tercera condición es el valor que se devolverá si no falta el valor de la columna Age del
          # conjunto de datos.
                    Dataset$Age)
+View(Dataset)
 
 # Hacemos lo mismo para la columna Salary
 Dataset$Salary = ifelse(is.na(Dataset$Salary),
                         ave(Dataset$Salary,FUN = function(x)mean(x,na.rm=TRUE)),
                         Dataset$Salary)
+View(Dataset)
+
+# ------------------------------------------------------------------------------
+# Paso 3: Codificación de datos categóricos
+# Se refiere a transformación de datos de texto en datos numéricos.
+# Para transformar una variable categórica en numérica, usamos la función factor().
+
+Dataset$Country = factor(Dataset$Country,
+                         levels = c('France','Spain','Germany'),
+                         labels = c(1.0, 2.0 , 3.0))
+View(Dataset)
+
+# Hacemos lo mismo para la columna purchased
+Dataset$Purchased = factor(Dataset$Purchased,
+                           levels = c('Yes','No'),
+                           labels = c(1,0))
+
+# Busca los valores NA y los reemplaza por 0
+Dataset$Purchased[is.na(Dataset$Purchased)] <- 0
+# Convierte la columna "Purchased" en un factor nuevamente, 
+# Asegurando que los cambios realizados anteriormente se mantengan en la estructura de datos.
+as.factor(Dataset$Purchased)
+
+View(Dataset)
+
+
+
