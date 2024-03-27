3dfe8c5df691dcee856510309a282cc4bf02be2d
Jonatan Badillo <jonatanbadillo.19@gmail.com>
2024-03-27 12:30:12 -0600

    ejecucion naive Bayes

--- Naive Bayes/ejemploNaiveBayes.R
+++ Naive Bayes/ejemploNaiveBayes.R
@@ -12,5 +12,29 @@ library(e1071)
 delays.df <- read.csv("FlightDelays.csv")
 
 # se cambian primero las variables numericas a categoricas
+# representan días de la semana y horas de salida, respectivamente, y se prefieren como 
+# variables categóricas en lugar de numéricas para evitar que los modelos de aprendizaje
+# los interpreten como valores numéricos continuos.
 delays.df$DAY_WEEK <- factor(delays.df$DAY_WEEK)
 delays.df$DEP_TIME <- factor(delays.df$DEP_TIME)
+
+
+# se crea la hora de salida de los contenedores(bins) por hora
+delays.df$CRS_DEP_TIME <- factor(round(delays.df$CRS_DEP_TIME/100))
+
+
+# se crean conjuntos de entrenamiento y validacion
+# contiene los índices de las variables seleccionadas. Estos índices corresponden a columnas específicas en el dataframe
+selected.var <- c(10, 1, 8, 4, 2, 13)
+# Crea un conjunto de índices de muestra para el conjunto de entrenamiento (train.index).
+
+#  Genera un conjunto de índices de muestra aleatorios (train.index) que representa el 60% del no. total de filas en el dataframe delays.df.
+train.index <- sample(c(1:dim(delays.df)[1]), dim(delays.df)[1]*0.6)
+
+# train.df contiene una parte de los datos originales para entrenamiento, 
+# valid.df contiene la parte restante de los datos para validación.
+train.df <- delays.df[train.index, selected.var]
+valid.df <- delays.df[-train.index, selected.var]
+
+
+
