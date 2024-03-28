5e3cd27479916d47d80aa01c718a359ecd3e35fa
Jonatan Badillo <jonatanbadillo.19@gmail.com>
2024-03-28 15:02:21 -0600

    entrenamiento y prueba

--- KNN/Tarea KNN/Tarea-KNN.R
+++ KNN/Tarea KNN/Tarea-KNN.R
@@ -29,3 +29,26 @@ normalize <- function(x) return( (x-min(x))/(max(x)-min(x)))
 
 # Aplicar la función de normalización a todas las columnas del conjunto de datos
 BostonHousing_normalized <- as.data.frame(lapply(BostonHousing, normalize))
+
+
+# Dividimos en conjunto de prueba y entrenamiento
+# Entrenamiento 67%
+# Prueba 33%
+bth <- sample(2,nrow(BostonHousing_normalized),replace = T,prob = c(0.67,0.33))
+
+# Seleccionamos las columnas necesarias
+boston.training <- BostonHousing_normalized[bth==1,1:12]
+boston.test <- BostonHousing_normalized[bth==2,1:12]
+
+class(boston.training)# es un data frame que necesitamo convertirlo a matriz
+
+# Convertir las variables a matrices de datos simples
+boston.training <- as.matrix(boston.training)
+boston.test <- as.matrix(boston.test)
+
+# Seleccionamos nuestra clase:
+boston.trainLabel <- BostonHousing_normalized[bth==1,13]
+boston.testLabel <- BostonHousing_normalized[bth==2,13]
+
+
+
