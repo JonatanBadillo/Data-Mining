42f5c32fe1424e971ec47d9f2fbad3193b72caf0
Jonatan Badillo <jonatanbadillo.19@gmail.com>
2024-03-28 14:05:31 -0600

    Tarea Boston-Housing

---
+++ KNN/Tarea KNN/Tarea-KNN.R
@@ -0,0 +1,9 @@
+# Tarea KNN
+# Predecir los precios medios de la vivienda
+
+# El archivo BostonHousing.csv contiene información sobre más de 500 secciones censales en
+# Boston, donde para cada sección se registran múltiples variables. La última columna
+# (CAT.MEDV) se derivó de MEDV, de modo que obtiene el valor 1 si MEDV > 30 y 0 en
+# caso contrario. Considera el objetivo de predecir el valor mediano (MEDV) de un tramo,
+# dada la información en las primeras 12 columnas. Divide los datos en conjuntos de
+# entrenamiento (60%) y validación (40%).
