e5e67e16f05eacaf1717dd4db65d267baa7655a0
Jonatan Badillo <jonatanbadillo.19@gmail.com>
2024-04-13 12:54:17 -0600

    Cargamos el conjunto de datos del zoológico

--- Arboles de Decision/ArbolesDecisiones.R
+++ Arboles de Decision/ArbolesDecisiones.R
@@ -1,1 +1,17 @@
 # Construir un árbol de decisiones con rpart y cómo ajustar sus hiperparámetros.
+
+# Imagina que trabajas en la participación pública en un santuario de vida silvestre.
+# Tienes la tarea de crear un juego interactivo para niños para enseñarles sobre
+# diferentes clases de animales.
+
+# El juego pide a los niños que piensen en cualquier animal del santuario y luego les
+# haces preguntas sobre las características físicas de ese animal. Según las respuestas
+# que dé el niño, el modelo debe decirle a qué clase pertenece su animal (mamífero, ave, reptil, etc.).
+
+# cargando los paquetes mlr y tidyverse:
+library(mlr)
+library(tidyverse) 
+# Cargamos el conjunto de datos del zoológico integrado en el paquete mlbench, convirtámoslo en un tibble y exploremos.
+data(Zoo, package = "mlbench")
+zooTib <- as_tibble(Zoo)
+zooTib
