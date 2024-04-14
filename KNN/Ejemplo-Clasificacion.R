07fb107ba0edde98627856f1977c35345a82aec7
Jonatan Badillo <jonatanbadillo.19@gmail.com>
2024-04-14 12:19:40 -0600

    Añadir sección de validación cruzada de nuestro modelo k-NN

--- KNN/Ejemplo-Clasificacion.R
+++ KNN/Ejemplo-Clasificacion.R
@@ -113,9 +113,71 @@ knn <- makeLearner("classif.knn", par.vals = list("k" = 2))
 # ------------------------------------------------------------------------------
 # Poniendo todo junto: Entrenando el modelo
 
+# Creamos nuestro modelo
 # Esto se logra con la función train(), que toma al aprendiz como primer argumento y la tarea
 # como segundo argumento:
 knnModel <- train(knn, diabetesTask)
+
+
+# Tenemos nuestro modelo, así que pasemos los datos a través de él para ver cómo funciona.
+# La función predict() toma datos sin etiquetar y los pasa a través del modelo para obtener las
+# clases predichas. El primer argumento es el modelo, y los datos que se le pasan se dan como
+# el argumento newdata:
+knnPred <- predict(knnModel, newdata = diabetesTib)
+
+
+# Resumen de las funciones predict() y performance() de mlr. 
+
+# predict() pasa las
+# observaciones a un modelo y genera los valores predichos. 
+
+# performance() compara estos
+# valores predichos con los valores verdaderos de los casos y genera una o más métricas de
+# rendimiento que resumen la similitud entre los dos.
+
+
+
+
+# Especificamos qué métricas de rendimiento queremos que devuelva la función
+# proporcionándolas como una lista al argumento de medidas. Las dos medidas que
+# contemplamos son mmce, el error medio de clasificación errónea; y acc, o exactitud. MMCE
+# es simplemente la proporción de casos clasificados como una clase diferente a su verdadera
+# clase. La precisión es lo opuesto a esto: la proporción de casos que el modelo clasificó
+# correctamente. Puedes ver que los dos suman 1.00:
+  
+
+performance(knnPred, measures = list(mmce, acc))
+# ¡Así que nuestro modelo clasifica correctamente el 95.86% de los casos!
+
+
+
+# ------------------------------------------------------------------------------
+# Validación cruzada de nuestro modelo k-NN
+# hagamos una validación cruzada del aprendiz.
+
+
+# Validación cruzada de retención (holdout)
+# Holdout CV es el método más simple de entender: simplemente “retiene” una proporción
+# aleatoria de tus datos como su conjunto de prueba y entrena tu modelo con los datos restantes.
+# Luego pasa el conjunto de prueba a través del modelo y calcula sus métricas de rendimiento
+# (hablaremos de esto pronto).
+# El conjunto de entrenamiento se usa para entrenar el modelo, que
+# luego se usa para hacer predicciones en el conjunto de prueba. La similitud de las
+# predicciones con los valores reales del conjunto de prueba se utiliza para evaluar el
+# rendimiento del modelo.
+
+# El primer paso al emplear cualquier CV en mlr es hacer una descripción de remuestreo, que
+# es simplemente un conjunto de instrucciones sobre cómo se dividirán los datos en conjuntos
+# de prueba y entrenamiento. El primer argumento de la función makeResampleDesc() es el
+# método CV que vamos a utilizar: en este caso, "Holdout". Para el CV de retención,
+# necesitamos decirle a la función qué proporción de los datos se usará como conjunto de
+# entrenamiento, por lo que proporcionamos esto al argumento de división:
+holdout <- makeResampleDesc(method = "Holdout", split = 2/3,
+                            stratify = TRUE)# TRUE. Le pide a la función que
+# se asegure de que cuando divida los datos en conjuntos de entrenamiento y prueba, intente
+# mantener la proporción de cada clase de paciente en cada conjunto.
+
+
 
 
 
