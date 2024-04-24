96ca7ddcc3e13a51c8131209353b113eb843cd15
Jonatan Badillo <jonatanbadillo.19@gmail.com>
2024-04-24 10:04:47 -0600

    data frame

--- Arboles de Decision/Ejemplo/Arboles-Decision-Ejemplo.R
+++ Arboles de Decision/Ejemplo/Arboles-Decision-Ejemplo.R
@@ -1,1 +1,17 @@
 # Ejemplo 1
+
+# Existe una estafa común entre los
+automovilistas por la cual una persona frena bruscamente en medio del tráfico
+pesado con la intención de que le choquen por detrás.
+• La persona luego presentará una reclamación de seguro por lesiones personales y
+daños a su vehículo, alegando que el otro conductor tuvo la culpa.
+• Supón que queremos predecir cuáles de los reclamos de una compañía de
+seguros son fraudulentos utilizando un árbol de decisiones.
+• Para empezar, necesitamos crear un conjunto de formación de reclamaciones
+fraudulentas conocidas.
+> train <- data.frame(
+  + ClaimID = c(1,2,3),
+  + RearEnd = c(TRUE, FALSE, TRUE),
+  + Fraud = c(TRUE, FALSE, TRUE)
+  + )
+> train
