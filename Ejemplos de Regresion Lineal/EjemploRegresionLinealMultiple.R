0bcdf26d89ba3c75221526f403dd707b4dfc21ed
Jonatan Badillo <jonatanbadillo.19@gmail.com>
2024-05-01 13:29:24 -0600

    Agregar análisis de regresión lineal múltiple en R

--- Ejemplos de Regresion Lineal/EjemploRegresionLinealMultiple.R
+++ Ejemplos de Regresion Lineal/EjemploRegresionLinealMultiple.R
@@ -8,3 +8,6 @@ Performance <- c(56, 74, 56, 81, 75, 84, 68, 52, 57, 82, 73, 90, 67, 79, 70, 66)
 # Luego usa los dos vectores para crear un frame de datos
 FarMisht.frame <- data.frame(Aptitude,Performance)
 FarMisht.frame
+
+# La función lm() (modelo lineal, se utiliza para ajustar modelos de regresión lineal en R.) realiza el análisis:
+FM.reg <-lm(Performance ~ Aptitude, data=FarMisht.frame) # estamos tratando de predecir el rendimiento en función de la aptitud.
