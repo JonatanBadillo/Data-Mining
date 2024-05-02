863a5b0476cb7cfc0d94a74f0ff063bc8843698e
Jonatan Badillo <jonatanbadillo.19@gmail.com>
2024-05-02 13:14:41 -0600

    Agregar cambios en el ejemplo de regresión lineal con estadísticas en R

--- Ejemplo de regresión lineal con estadísticas/Ejemplo-Regresion-Lineal.R
+++ Ejemplo de regresión lineal con estadísticas/Ejemplo-Regresion-Lineal.R
@@ -127,3 +127,142 @@ dim(train)
 
 # Puedes ver que tenemos el 70 % de las observaciones aleatorias en el conjunto de datos de
 # entrenamiento.
+
+
+
+# Construcción del modelo
+# Training model
+lmModel <- lm(Price ~ . , data = train) #  especifica que la variable dependiente es Price, y el . indica que todas las demás variables en el conjunto de datos train se utilizarán como variables independientes para predecir Price.
+# Printing the model object
+print(lmModel)
+
+
+
+
+
+# Interpretación de los coeficientes de regresión
+
+# En el resultado anterior, Intercept representa el valor mínimo de Price que se recibirá, si todas
+# las variables son constantes o están ausentes.
+# Ten en cuenta
+# La intercepción no siempre tiene sentido en términos comerciales.
+# La pendiente (representada por variables independientes) nos informa sobre la tasa de cambio
+# que presenciará la variable Precio (Price), con cada cambio de una unidad en la variable
+# independiente. Por ejemplo, si AreaHouse de la casa aumenta en una unidad más, el Precio
+# de la casa aumentará en 165,637.
+
+
+
+
+# Validación de modelos y coeficientes de regresión
+
+# En R, la función lm ejecuta una prueba t de una muestra contra cada
+# coeficiente beta para garantizar que sean significativos y que no se hayan producido por
+# casualidad. Del mismo modo, necesitamos validar el modelo general. Al igual que una prueba
+# t de una muestra, la función lm también genera tres estadísticas, que ayudan a los científicos
+# de datos a validar el modelo. Estas estadísticas incluyen R-Square, R-Square ajustado y F-
+#   test, también conocidas como pruebas globales.
+
+# Para ver estas estadísticas, necesitamos pasar el objeto lmModel a la función summary().
+summary(lmModel)
+
+
+
+
+# En el resultado anterior, Pr(>|t|) representa el valor p, que se puede comparar con el valor
+# alfa de 0.05 para garantizar si el coeficiente beta correspondiente es significativo o no. La
+# función lm aquí ayuda. Todos los valores en la salida que tienen (.) punto o (*) asterisco
+# contra los nombres de las variables indican que estos valores son significativos. Con base en
+# esto, ahora sabemos que todas las variables son estadísticamente significativas excepto
+# Avg..AreaNumberofBedrooms.
+
+# Para la precisión general del modelo, analicemos las estadísticas generadas por la función lm
+# una por una.
+
+# 1. R-cuadrado múltiple: 0.919: El valor de R-cuadrado se denomina formalmente coeficiente
+# de determinación. Aquí, 0.919 indica que las variables intersección, Ingreso de área, Casa de
+# área, Número de habitaciones de área y Población de área, cuando se juntan, pueden explicar
+# el 91.9 % de la varianza en la variable Precio. El valor de R-cuadrado se encuentra entre 0 y
+# 1. En aplicaciones prácticas, si el valor de R2 es superior a 0.70, lo consideramos un buen
+# modelo.
+
+# 2. R-cuadrado ajustado: 0.9189: El valor de R-cuadrado ajustado indica si la adición de nueva
+# información (variable) aporta una mejora significativa al modelo o no. Así que, a partir de
+# ahora, este valor no proporciona mucha información. Sin embargo, el aumento en el valor de
+# R-cuadrado ajustado con la adición de una nueva variable indicará que la variable es útil y
+# aporta una mejora significativa al modelo.
+# No se aprecia una gran diferencia entre el R-cuadrado y el R-cuadrado ajustado y
+# generalmente indica que existe multicolinealidad dentro de los datos.
+
+# 3. Estadístico F: 7925 en 5 y 3494 DF, p-value: < 2.2e-16 – Esta línea habla sobre la prueba
+# global del modelo. La función lm ejecuta una prueba ANOVA para verificar la importancia
+# del modelo general. Aquí la hipótesis nula es que el modelo no es significativo, y la
+# alternativa es que el modelo es significativo. Según los valores de p < 0.05, nuestro modelo
+# es significativo.
+
+
+
+
+# Sin embargo, mirar estas estadísticas es suficiente para tomar una decisión sobre la
+# importancia del modelo. Pero existen otros métodos de validación para la regresión lineal
+# que pueden ser de ayuda al decidir qué tan bueno o malo es el modelo. Algunos de ellos se
+# mencionan a continuación:
+
+#   4. Valores AIC y BIC: el AIC (criterio de información de Akaike, 1974) y el BIC (criterio de
+#   información bayesiano, 1978) son criterios de probabilidad penalizada. Ambas medidas
+# utilizan una “medida de ajuste + penalización por complejidad” para obtener los valores
+# finales.
+
+# AIC = – 2 * ln(likelihood) + 2 * p
+# BIC = – 2 * ln(likelihood) + ln(N) * p
+
+# Aquí p = número de parámetros estimados y N = tamaño de muestra.
+# Los valores AIC y BIC se pueden usar para elegir los mejores subconjuntos de predictores
+# en regresión y para comparar diferentes modelos. Al comparar diferentes modelos, el modelo
+# con valores mínimos de AIC y BIC se considera el mejor modelo.
+# Nota:
+#   Es probable que AIC sobreajuste los datos, mientras que BIC probablemente no ajuste los
+# datos.
+
+AIC(lmModel)
+
+BIC(lmModel)
+
+
+# 5. Error cuadrático medio (RMSE): Al comparar las estadísticas de RMSE de diferentes
+# modelos, podemos decidir cuál es un mejor modelo. El modelo con el valor RMSE más bajo
+# se considera un mejor modelo. Hay otras funciones similares como MAE, MAPE, MSE, etc.
+# que se pueden usar. Estas funciones se pueden encontrar en Metrics R Package. Estas
+# funciones toman principalmente dos argumentos: uno es el valor real y el segundo, los valores
+# predichos. Así que veamos cómo podemos obtener estos valores. Los datos reales se pueden
+# encontrar al 100 % a partir del conjunto de datos original o de los datos de entrenamiento en
+# nuestro caso. Sin embargo, para encontrar los valores ajustados, necesitamos explorar el
+# objeto modelo.
+
+names(lmModel)
+
+# El vector anterior presenta los nombres de los objetos que constituyen el objeto modelo.
+# Aquí, los valores ajustados son los valores predichos. Ahora, usaremos estos valores para
+# generar los valores rmse.
+
+library(Metrics)
+rmse(actual = train$Price, predicted = lmModel$fitted.values)
+
+
+
+
+# Comprobación de supuestos de regresión lineal
+
+# La regresión lineal es paramétrica, lo que significa que el algoritmo hace algunas
+# suposiciones sobre los datos. Un modelo de regresión lineal solo se considera adecuado si se
+# cumplen estos supuestos. Hay alrededor de cuatro suposiciones y se mencionan a
+# continuación. Si el modelo no cumple con estos supuestos, entonces simplemente no
+# podemos usar este modelo.
+
+# 1. Los errores deben seguir una distribución normal: Esto se puede verificar dibujando
+# un histograma de residuos o usando la función plot(). La función plot crea 4 gráficos
+# diferentes. Uno de los cuales es un diagrama de NPP. El gráfico confirma si los errores siguen
+# una distribución normal o no.
+
+# Generando histograma
+hist(lmModel$residuals, color = "grey")
