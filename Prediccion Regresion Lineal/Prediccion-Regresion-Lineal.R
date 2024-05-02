f9c1711056f6b6dd69a6765494dfd1c06ca5069d
Jonatan Badillo <jonatanbadillo.19@gmail.com>
2024-05-02 17:30:04 -0600

    prediccion regresion lineal

---
+++ Prediccion Regresion Lineal/Prediccion-Regresion-Lineal.R
@@ -0,0 +1,7 @@
+# Predicción con regresión lineal
+
+# Para iniciar, necesitamos instalar los siguientes paquetes (si ya está instalado, solo cárgalo!):
+library(tidyverse)
+library(mlr)
+library(imputeTS)
+library(prettydoc)
