ca180b6ccfafccfaa802cc184c51b05b627c9b2c
Jonatan Badillo <jonatanbadillo.19@gmail.com>
2024-05-13 13:24:25 -0600

    regresion con SVM

---
+++ Regresion SVM/Ejemplo-Regresion-SVM.R
@@ -0,0 +1,10 @@
+# Ejemplo de regresión con máquinas de soporte vectorial
+
+# Primero haremos una regresión lineal simple, luego pasaremos a la regresión con SVM para
+# que puedas ver cómo se comportan los dos con los mismos datos.
+
+
+# Un conjunto de datos simple
+# Para empezar, usaremos este conjunto de datos simple:
+
+
