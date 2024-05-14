1fd9e613436f9d8cacf8241bf267b9d45c193815
Jonatan Badillo <jonatanbadillo.19@gmail.com>
2024-05-13 18:44:51 -0600

    Agregar visualización de predicciones en el conjunto de datos de regresión lineal

--- Regresion SVM/SVM-Clasificacion-Multiclase.R
+++ Regresion SVM/SVM-Clasificacion-Multiclase.R
@@ -30,3 +30,17 @@ pred <- predict(model, x, decision.values = TRUE)
 attr(pred, "decision.values")[1:4,]
 # visualize (classes by color, SV by crosses):
 plot(cmdscale(dist(iris[,-5])),col = as.integer(iris[,5]),pch = c("o","+")[1:150 %in% model$index + 1])
+
+
+
+## try regression mode on two dimensions
+# create data
+x <- seq(0.1, 5, by = 0.05)
+y <- log(x) + rnorm(x, sd = 0.2)
+# estimate model and predict input values
+m <- svm(x, y)
+new <- predict(m, x)
+# visualize
+plot(x, y)
+points(x, log(x), col = 2)
+points(x, new, col = 4)
