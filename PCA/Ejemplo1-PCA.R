a62a4a9e9925caf5e638a0fda1f1b2db024d9a48
Jonatan Badillo <jonatanbadillo.19@gmail.com>
2024-05-15 14:02:44 -0600

    chore: Agregar análisis descriptivo de datos en archivo de ejemplo de PCA

--- PCA/Ejemplo1-PCA.R
+++ PCA/Ejemplo1-PCA.R
@@ -8,3 +8,21 @@
 # vecindario, que tenía una tasa de criminalidad per cápita promedio de 0.006, 18% de la tierra
 # residencial dividida en zonas para lotes de más de 25,000 pies2, 2.31% de la tierra dedicada
 # a negocios no minoristas, sin frontera con el río Charles, y pronto.
+
+boston.housing.df <- read.csv("BostonHousing.csv", header = TRUE)
+head(boston.housing.df, 9)
+summary(boston.housing.df)
+
+mean(boston.housing.df$CRIM)
+# find the number of missing values of variable CRIM
+sum(is.na(boston.housing.df$CRIM))
+
+# data frame que contiene las estadísticas descriptivas mencionadas 
+data.frame(mean=sapply(boston.housing.df, mean),
+           sd=sapply(boston.housing.df, sd),
+           min=sapply(boston.housing.df, min),
+           max=sapply(boston.housing.df, max),
+           median=sapply(boston.housing.df, median),
+           length=sapply(boston.housing.df, length),
+           miss.val=sapply(boston.housing.df, function(x)
+           sum(length(which(is.na(x))))))
