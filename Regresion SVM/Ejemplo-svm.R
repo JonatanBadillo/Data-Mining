1e536d63c78bbf5e61788d44102afa5bf4c1d35e
Jonatan Badillo <jonatanbadillo.19@gmail.com>
2024-05-15 17:28:23 -0600

    Agregar funcionalidad de trazado de datos y ajuste de modelo SVM

--- Regresion SVM/Ejemplo-svm.R
+++ Regresion SVM/Ejemplo-svm.R
@@ -1,3 +1,12 @@
+# establecer el generador de números pseudoaleatorios
+set.seed (10)
+# Adjuntar Paquetes
+library (tidyverse) # manipulación y visualización de datos
+library (kernlab) # metodología SVM
+library (e1071) # metodología SVM
+library (ISLR) # contiene un conjunto de datos de ejemplo "Khan"
+library (RColorBrewer) # coloración personalizada de las parcelas
+
 # Clasificador de margen máximo
 
 # Si las clases son separables por un límite lineal, podemos usar un clasificador de margen máximo para
@@ -10,3 +19,23 @@ x <- matrix (rnorm (20 * 2), ncol = 2)
 y <- c (rep (-1,10), rep (1,10))
 x [y == 1,] <- x [y == 1,] + 3/2
 dat <- data.frame (x = x, y = as.factor (y))
+
+# Trazar datos
+ggplot(data = dat, aes(x = x.2, y = x.1, color = y, shape = y)) +
+  geom_point(size = 2) +
+  scale_color_manual(values=c("#000000", "#FF0000")) +
+  theme(legend.position = "none")
+
+# 
+# El objetivo del clasificador de margen máximo es identificar el límite lineal que maximiza la distancia total
+# entre la línea y el punto más cercano en cada clase. Podemos usar la función svm() en el paquete e1071 para
+# encontrar este límite.
+
+# Ajustar el modelo de la máquina de vectores de soporte al conjunto de datos
+svmfit <- svm(y~., data = dat, kernel = "linear", scale = FALSE)
+# Plot Results
+plot(svmfit, dat)
+
+
+
+
