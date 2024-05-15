399d8f8eec6da2c5cc018241f42776a383befb6a
Jonatan Badillo <jonatanbadillo.19@gmail.com>
2024-05-15 13:57:13 -0600

    Agregar archivo de ejemplo para análisis de componentes principales (PCA)

---
+++ PCA/Ejemplo1-PCA.R
@@ -0,0 +1,10 @@
+# Ejemplo 1: Precios de la vivienda en Boston
+
+# Retomamos el ejemplo que se propone en la tarea de regresión lineal múltiple, Boston
+# Housing data set. Para cada vecindario, se dan varias variables, como la tasa de criminalidad,
+# la proporción de estudiantes por maestro y el valor medio de una unidad de vivienda en el
+# vecindario. En la tabla 1 se da una descripción de las 14 variables. Los primeros nueve
+# registros de los datos se muestran en la tabla 2. La primera fila representa el primer
+# vecindario, que tenía una tasa de criminalidad per cápita promedio de 0.006, 18% de la tierra
+# residencial dividida en zonas para lotes de más de 25,000 pies2, 2.31% de la tierra dedicada
+# a negocios no minoristas, sin frontera con el río Charles, y pronto.
