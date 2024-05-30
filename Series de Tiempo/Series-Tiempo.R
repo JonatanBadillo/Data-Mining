f83090909c35d4fbdbb6c4117244943310809d71
Jonatan Badillo <jonatanbadillo.19@gmail.com>
2024-05-30 13:14:23 -0600

    series de tiempo

---
+++ Series de Tiempo/Series-Tiempo.R
@@ -0,0 +1,24 @@
+# Series de tiempo y causalidad
+
+# Una serie de tiempo univariada es donde las mediciones se recopilan durante una medida de
+# tiempo estándar, que podría ser por minuto, hora, día, semana, mes, etc. Lo que hace que las
+# series temporales sean más problemáticas que otros datos es que el orden de las
+# observaciones importa. Esta dependencia del orden puede hacer que los métodos de análisis
+# estándar produzcan un sesgo o una variación innecesariamente altos.
+
+# Otro aspecto de las series temporales que a menudo se pasa por alto es la causalidad. Sí, no
+# queremos confundir correlación con causalidad, pero, en el análisis de series temporales,
+# podemos aplicar la técnica de causalidad de Granger para determinar si la causalidad,
+# estadísticamente hablando, existe.
+# Aplicaremos series de tiempo/técnicas econométricas para identificar
+# modelos de pronóstico univariados (incluidos conjuntos), modelos de vectores
+# autorregresivos y, finalmente, la causalidad de Granger. 
+
+# es posible que no domines completamente el análisis de series de tiempo, pero
+# sabrás lo suficiente para realizar un análisis efectivo y comprender las cuestiones
+# fundamentales a considerar al construir modelos de series de tiempo y crear modelos
+# predictivos (pronósticos).
+# Los siguientes son los temas que se cubrirán:
+# • Análisis univariado de series de tiempo
+# • Datos de series de tiempo
+# • Modelado y evaluación
