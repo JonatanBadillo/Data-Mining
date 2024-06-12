cdb50a946e91b8efd7edd3fffdf544b5ad42adfd
Jonatan Badillo <jonatanbadillo.19@gmail.com>
2024-06-12 13:46:06 -0600

    chore: clean data and convert specific values to NA

--- Datos/Limpieza/limpieza.R
+++ Datos/Limpieza/limpieza.R
@@ -11,8 +11,16 @@ head(data)
 
 summary(data)
 
+# Hasta la fila 8760, que abarca has 31/12/23
+data <- data[1:8760, ]
+
+
 # Convertir valores específicos a NA
 data[data == "F.O." | data == "D.I." | data == "Mtto." | data == "Mtto"] <- NA
+
+
+na_count <- colSums(is.na(data))
+na_count
 
 # Ver las primeras filas del dataframe
 head(data)
@@ -29,6 +37,9 @@ data <- data %>%
 # Verificar los cambios
 head(data)
 
+
+na_count <- colSums(is.na(data))
+na_count
 
 # Guardar el dataframe limpio en un nuevo archivo CSV
 write.csv(data, "~/Desktop/UNIVERSITY/Servicio-Social/Data-Mining/Datos/NINFAS/NINFAS-2023-limpiado.csv", row.names = FALSE)
