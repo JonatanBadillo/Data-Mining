ae1454b5e65b900b020b771974e7492cb2df8978
Jonatan Badillo <jonatanbadillo.19@gmail.com>
2024-06-16 20:17:04 -0600

    chore: Update clustering script to determine optimal number of clusters using elbow method

--- Clustering a los datos/cluster.R
+++ Clustering a los datos/cluster.R
@@ -10,22 +10,27 @@ data$SO2 <- as.numeric(data$SO2)
 data$`PM-10` <- as.numeric(data$`PM-10`)
 data$`PM-2.5` <- as.numeric(data$`PM-2.5`)
 
-# Revisar los primeros datos para confirmar que están bien cargados
+# revisar los primeros datos para confirmar que están bien cargados
 head(data)
 
 # Normalizar las columnas numéricas
 data_scaled <- scale(data[, c("O3", "NO2", "CO", "SO2", "PM-10", "PM-2.5")])
 
 
-# Paso 3: Aplicar k-means clustering
+# Aplicar k-means clustering
+
 # aplicaremos k-means clustering a los datos normalizados. 
 # decidir el número de clusters (k), utilizando el método del codo (elbow method).
-
+# calcular la suma de los cuadrados dentro de los clusters (WSS) para diferentes valores de k (número de clusters). 
 # Método del codo para determinar el número óptimo de clusters
 set.seed(123) 
 # suma de los cuadrados por cada grupo : wss
 wss <- (nrow(data_scaled)-1)*sum(apply(data_scaled, 2, var)) 
 for (i in 2:15) wss[i] <- sum(kmeans(data_scaled, centers=i)$withinss)
+
+
+
+
 
 # Graficar el método del codo
 # El "codo" en la gráfica dará una idea del número óptimo de clusters. Supongamos que decidimos usar 3 clusters.
@@ -35,7 +40,7 @@ plot(1:15, wss, type="b", xlab="Número de Clusters", ylab="Suma de Cuadrados De
 
 
 # Aplicar k-means con 4 clusters
-set.seed(123) # Para reproducibilidad
+set.seed(123) 
 kmeans_result <- kmeans(data_scaled, centers=4)
 
 # Agregar la asignación de clusters a los datos originales
@@ -55,4 +60,5 @@ pairs(data[, c("O3", "NO2", "CO", "SO2", "PM-10", "PM-2.5")], col=data$cluster, 
 
 # Calcular la media de cada variable para cada cluster
 aggregate(data[, c("O3", "NO2", "CO", "SO2", "PM-10", "PM-2.5")], by=list(cluster=data$cluster), FUN=mean)
+
 
