40e1803d80fe53ed2ac6a01e4f527dcbf97d222e
Jonatan Badillo <jonatanbadillo.19@gmail.com>
2024-06-16 19:49:48 -0600

    chore: Update clustering script to determine optimal number of clusters using elbow method

--- Clustering a los datos/cluster.R
+++ Clustering a los datos/cluster.R
@@ -17,4 +17,18 @@ head(data)
 data_scaled <- scale(data[, c("O3", "NO2", "CO", "SO2", "PM-10", "PM-2.5")])
 
 
+# Paso 3: Aplicar k-means clustering
+# aplicaremos k-means clustering a los datos normalizados. 
+# decidir el número de clusters (k), utilizando el método del codo (elbow method).
+
+# Método del codo para determinar el número óptimo de clusters
+set.seed(123) 
+wss <- (nrow(data_scaled)-1)*sum(apply(data_scaled, 2, var))
+for (i in 2:15) wss[i] <- sum(kmeans(data_scaled, centers=i)$withinss)
+
+# Graficar el método del codo
+# El "codo" en la gráfica te dará una idea del número óptimo de clusters. Supongamos que decidimos usar 3 clusters.
+plot(1:15, wss, type="b", xlab="Número de Clusters", ylab="Suma de Cuadrados Dentro de los Clusters (WSS)")
+
+# sugiere que lo ideal sean hasta 4 o 5 clusters
 
