c639f520a2f9955152a9f9a07c5783f2ee3c17fd
Jonatan Badillo <jonatanbadillo.19@gmail.com>
2024-06-19 14:15:25 -0600

    feat: Agregar gráfico de líneas para la concentración de contaminantes por hora en Puebla

--- Series Tiempo, contaminantes/series-tiempo.R
+++ Series Tiempo, contaminantes/series-tiempo.R
@@ -15,3 +15,20 @@ data <- data %>%
   mutate(Datetime = dmy_hms(paste(FECHA, Horas))) %>%
   select(-FECHA, -Horas) %>%
   arrange(Datetime)
+
+
+# Derretir los datos para hacerlos long format (formato largo)
+data_long <- data %>%
+  pivot_longer(cols = c(O3, NO2, CO, SO2, `PM-10`, `PM-2.5`), 
+               names_to = "Contaminante", 
+               values_to = "Concentracion")
+
+# Crear el gráfico de líneas
+ggplot(data_long, aes(x = Datetime, y = Concentracion, color = Contaminante)) +
+  geom_line() +
+  facet_wrap(~ Contaminante, scales = "free_y") +
+  labs(title = "Concentración de contaminantes por hora en Puebla",
+       x = "Fecha y Hora",
+       y = "Concentración") +
+  theme_minimal() +
+  theme(legend.position = "none")
