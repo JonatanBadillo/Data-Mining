596e15dc3e9115efda15f1deee0c16e41c3fbf48
Jonatan Badillo <jonatanbadillo.19@gmail.com>
2024-06-20 13:44:49 -0600

    Actualizar gráficos de líneas y barras para la concentración de contaminantes por hora en Puebla

--- SeriesTiempo-contaminantes/series-tiempo.R
+++ SeriesTiempo-contaminantes/series-tiempo.R
@@ -42,16 +42,41 @@ NINFAS2023_hora_contaminantes <- ggplot(promedio_hora, aes(x = Hora, y = Promedi
        y = "Promedio de concentración") +
   theme_minimal()
 
-# Crear la carpeta 'imagenes' si no existe
-if (!dir.exists("imagenes")) {
-  dir.create("imagenes")
-}
-
 # Guardar el gráfico en la carpeta 'imagenes'
 ggsave("~/Desktop/UNIVERSITY/Servicio-Social/Data-Mining/SeriesTiempo-contaminantes/imagenes/NINFAS2023/Todos-Contaminantes/NINFAS2023_hora_contaminantes.jpg", NINFAS2023_hora_contaminantes, width = 10, height = 6, dpi = 300)
 
 
+# Iterar sobre cada contaminante para generar y guardar un gráfico
+contaminantes_unicos <- unique(promedio_hora$Contaminante)
 
+# Ruta base para guardar los gráficos
+ruta_base <- "~/Desktop/UNIVERSITY/Servicio-Social/Data-Mining/SeriesTiempo-contaminantes/imagenes/NINFAS2023/Cada-Contaminantes"
+
+# Crear la carpeta si no existe
+if (!dir.exists(ruta_base)) {
+  dir.create(ruta_base, recursive = TRUE)
+}
+
+for(contaminante in contaminantes_unicos) {
+  # Filtrar los datos para el contaminante actual
+  datos_contaminante <- filter(promedio_hora, Contaminante == contaminante)
+  
+  # Crear el gráfico
+  grafico <- ggplot(datos_contaminante, aes(x = Hora, y = Promedio_Concentracion)) +
+    ylim(c(min_value, max_value)) + 
+    geom_line(size = 1) +
+    scale_x_continuous(breaks = 0:23, limits = c(0, 23)) + # Asegura que se muestren todas las horas
+    labs(title = paste("Promedio de concentración de", contaminante, "por hora en Puebla NINFAS 2023"),
+         x = "Hora del día",
+         y = "Promedio de concentración") +
+    theme_minimal()
+  
+  # Definir el nombre del archivo basado en el contaminante
+  nombre_archivo <- paste(ruta_base, "/NINFAS2023_hora_", tolower(gsub(" ", "_", contaminante)), ".jpg", sep = "")
+  
+  # Guardar el gráfico en la ruta especificada
+  ggsave(nombre_archivo, grafico, width = 10, height = 6, dpi = 300)
+}
 
 
 
