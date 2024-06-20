64489066cac911c7bbedb9d9cdfc498df80262b0
Jonatan Badillo <jonatanbadillo.19@gmail.com>
2024-06-20 13:36:10 -0600

    Actualizar ruta de guardado del gráfico de concentración promedio por hora de contaminantes en Puebla

--- SeriesTiempo-contaminantes/series-tiempo.R
+++ SeriesTiempo-contaminantes/series-tiempo.R
@@ -18,13 +18,11 @@ data <- data %>%
   arrange(Datetime)
 
 
-# Derretir los datos para hacerlos long format (formato largo)
+# hacerlos long format (formato largo)
 data_long <- data %>%
   pivot_longer(cols = c(O3, NO2, CO, SO2, `PM-10`, `PM-2.5`), 
                names_to = "Contaminante", 
                values_to = "Concentracion")
-
-
 
 # Extraer la hora del datetime
 data_long <- data_long %>%
@@ -34,8 +32,6 @@ data_long <- data_long %>%
 promedio_hora <- data_long %>%
   group_by(Hora, Contaminante) %>%
   summarise(Promedio_Concentracion = mean(Concentracion, na.rm = TRUE))
-
-
 
 # Asignar el gráfico a una variable
 NINFAS2023_hora_contaminantes <- ggplot(promedio_hora, aes(x = Hora, y = Promedio_Concentracion, color = Contaminante, group = Contaminante)) +
@@ -52,7 +48,7 @@ if (!dir.exists("imagenes")) {
 }
 
 # Guardar el gráfico en la carpeta 'imagenes'
-ggsave("~/Desktop/UNIVERSITY/Servicio-Social/Data-Mining/SeriesTiempo-contaminantes/imagenes/NINFAS2023_hora_contaminantes.jpg", NINFAS2023_hora_contaminantes, width = 10, height = 6, dpi = 300)
+ggsave("~/Desktop/UNIVERSITY/Servicio-Social/Data-Mining/SeriesTiempo-contaminantes/imagenes/NINFAS2023/Todos-Contaminantes/NINFAS2023_hora_contaminantes.jpg", NINFAS2023_hora_contaminantes, width = 10, height = 6, dpi = 300)
 
 
 
