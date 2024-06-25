eefd38018b587a7e4ac59fa043e0f2c395e7a952
Jonatan Badillo <jonatanbadillo.19@gmail.com>
2024-06-25 14:35:39 -0600

    AGUA SANTA 2020-2024

---
+++ SeriesTiempo-contaminantes/union-graficas.R
@@ -0,0 +1,20 @@
+# AGUA SANTA
+library(dplyr)
+library(readr)
+
+# Definir la ruta a la carpeta que contiene los archivos CSV
+folder_path <- "~/Desktop/UNIVERSITY/Servicio-Social/Data-Mining/Datos/AGUA SANTA/limpios"
+
+# Listar todos los archivos CSV en la carpeta
+file_list <- list.files(path = folder_path, pattern = "*.csv", full.names = TRUE)
+
+# Leer y concatenar todos los archivos CSV en un solo DataFrame
+all_data <- lapply(file_list, read_csv) %>% bind_rows()
+
+
+
+# Exportar el DataFrame combinado a un nuevo archivo CSV
+write_csv(all_data, paste0(folder_path, "AGUA_SANTA_2020-2024_combinado.csv"))
+
+# Mostrar las primeras filas del DataFrame combinado
+head(all_data)
