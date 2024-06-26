785623bb64c3a15a15d50f165824ced7f6e1cd78
Jonatan Badillo <jonatanbadillo.19@gmail.com>
2024-06-26 12:45:47 -0600

    NINFAS data cleaning scripts and combine CSV files

--- SeriesTiempo-contaminantes/union-graficas.R
+++ SeriesTiempo-contaminantes/union-graficas.R
@@ -2,8 +2,31 @@
 library(dplyr)
 library(readr)
 
+data1 <- read_csv("Desktop/UNIVERSITY/Servicio-Social/Data-Mining/Datos/AGUA SANTA/limpios/AGUA-SANTA-2020-limpiado.csv")
+data2 <- read_csv("Desktop/UNIVERSITY/Servicio-Social/Data-Mining/Datos/AGUA SANTA/limpios/AGUA-SANTA-2021-limpiado.csv")
+data3 <- read_csv("Desktop/UNIVERSITY/Servicio-Social/Data-Mining/Datos/AGUA SANTA/limpios/AGUA-SANTA-2022-limpiado.csv")
+data4 <- read_csv("Desktop/UNIVERSITY/Servicio-Social/Data-Mining/Datos/AGUA SANTA/limpios/AGUA-SANTA-2023-limpiado.csv")
+data5 <- read_csv("Desktop/UNIVERSITY/Servicio-Social/Data-Mining/Datos/AGUA SANTA/limpios/AGUA-SANTA-2024-limpiado.csv")
+
+
+colnames(data1) <- c("FECHA", "Horas", "O3", "NO2", "CO", "SO2", "PM-10", "PM-2.5")
+colnames(data2) <- c("FECHA", "Horas", "O3", "NO2", "CO", "SO2", "PM-10", "PM-2.5")
+colnames(data3) <- c("FECHA", "Horas", "O3", "NO2", "CO", "SO2", "PM-10", "PM-2.5")
+colnames(data4) <- c("FECHA", "Horas", "O3", "NO2", "CO", "SO2", "PM-10", "PM-2.5")
+colnames(data5) <- c("FECHA", "Horas", "O3", "NO2", "CO", "SO2", "PM-10", "PM-2.5")
+
 # Definir la ruta a la carpeta que contiene los archivos CSV
-folder_path <- "~/Desktop/UNIVERSITY/Servicio-Social/Data-Mining/Datos/AGUA SANTA/limpios"
+folder_path <- "~/Desktop/UNIVERSITY/Servicio-Social/Data-Mining/Datos/AGUA SANTA/limpios/"
+
+# Exportar el DataFrame combinado a un nuevo archivo CSV
+write_csv(data1, paste0(folder_path, "AGUA-SANTA-2020-limpiado.csv"))
+write_csv(data2, paste0(folder_path, "AGUA-SANTA-2021-limpiado.csv"))
+write_csv(data3, paste0(folder_path, "AGUA-SANTA-2022-limpiado.csv"))
+write_csv(data4, paste0(folder_path, "AGUA-SANTA-2023-limpiado.csv"))
+write_csv(data5, paste0(folder_path, "AGUA-SANTA-2024-limpiado.csv"))
+
+
+
 
 # Listar todos los archivos CSV en la carpeta
 file_list <- list.files(path = folder_path, pattern = "*.csv", full.names = TRUE)
@@ -37,7 +60,30 @@ all_data <- lapply(file_list, read_csv) %>% bind_rows()
 
 
 # Exportar el DataFrame combinado a un nuevo archivo CSV
-write_csv(all_data, paste0(folder_path, "BINE_2020-2024_combinado.csv"))
+write_csv(all_data, paste0(folder_path, "BINE_2021-2024_combinado.csv"))
+
+# Mostrar las primeras filas del DataFrame combinado
+head(all_data)
+
+
+# -----------------------------------------
+# NINFAS
+library(dplyr)
+library(readr)
+
+# Definir la ruta a la carpeta que contiene los archivos CSV
+folder_path <- "~/Desktop/UNIVERSITY/Servicio-Social/Data-Mining/Datos/NINFAS/limpios"
+
+# Listar todos los archivos CSV en la carpeta
+file_list <- list.files(path = folder_path, pattern = "*.csv", full.names = TRUE)
+
+# Leer y concatenar todos los archivos CSV en un solo DataFrame
+all_data <- lapply(file_list, read_csv) %>% bind_rows()
+
+
+
+# Exportar el DataFrame combinado a un nuevo archivo CSV
+write_csv(all_data, paste0(folder_path, "NINFAS_2020-2024_combinado.csv"))
 
 # Mostrar las primeras filas del DataFrame combinado
 head(all_data)
