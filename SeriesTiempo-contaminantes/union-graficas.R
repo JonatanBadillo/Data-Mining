1048ac8a6b8f68ca46915bcba4a7f542e2b1f207
Jonatan Badillo <jonatanbadillo.19@gmail.com>
2024-06-26 13:14:32 -0600

    Actualizar limpieza de datos para BINE 2021-2024

--- SeriesTiempo-contaminantes/union-graficas.R
+++ SeriesTiempo-contaminantes/union-graficas.R
@@ -42,6 +42,10 @@ write_csv(all_data, paste0(folder_path, "AGUA_SANTA_2020-2024_combinado.csv"))
 # Mostrar las primeras filas del DataFrame combinado
 head(all_data)
 
+na_count <- colSums(is.na(all_data))
+na_count
+
+
 
 # -----------------------------------------
 # BINE
@@ -95,15 +99,38 @@ na_count
 # NINFAS
 library(dplyr)
 library(readr)
+library(tidyr)
+
+data1 <- read_csv("Desktop/UNIVERSITY/Servicio-Social/Data-Mining/Datos/NINFAS/limpios/NINFAS-2020-limpiado.csv")
+data2 <- read_csv("Desktop/UNIVERSITY/Servicio-Social/Data-Mining/Datos/NINFAS/limpios/NINFAS-2021-limpiado.csv")
+data3 <- read_csv("Desktop/UNIVERSITY/Servicio-Social/Data-Mining/Datos/NINFAS/limpios/NINFAS-2022-limpiado.csv")
+data4 <- read_csv("Desktop/UNIVERSITY/Servicio-Social/Data-Mining/Datos/NINFAS/limpios/NINFAS-2023-limpiado.csv")
+data5 <- read_csv("Desktop/UNIVERSITY/Servicio-Social/Data-Mining/Datos/NINFAS/limpios/NINFAS-2024-limpiado.csv")
+
+
+colnames(data1) <- c("FECHA", "Horas", "O3", "NO2", "CO", "SO2", "PM-10", "PM-2.5")
+colnames(data2) <- c("FECHA", "Horas", "O3", "NO2", "CO", "SO2", "PM-10", "PM-2.5")
+colnames(data3) <- c("FECHA", "Horas", "O3", "NO2", "CO", "SO2", "PM-10", "PM-2.5")
+colnames(data4) <- c("FECHA", "Horas", "O3", "NO2", "CO", "SO2", "PM-10", "PM-2.5")
+colnames(data5) <- c("FECHA", "Horas", "O3", "NO2", "CO", "SO2", "PM-10", "PM-2.5")
 
 # Definir la ruta a la carpeta que contiene los archivos CSV
-folder_path <- "~/Desktop/UNIVERSITY/Servicio-Social/Data-Mining/Datos/NINFAS/limpios"
+folder_path <- "~/Desktop/UNIVERSITY/Servicio-Social/Data-Mining/Datos/NINFAS/limpios/"
+
+# Exportar el DataFrame combinado a un nuevo archivo CSV
+write_csv(data1, paste0(folder_path, "NINFAS-2020-limpiado.csv"))
+write_csv(data2, paste0(folder_path, "NINFAS-2021-limpiado.csv"))
+write_csv(data3, paste0(folder_path, "NINFAS-2022-limpiado.csv"))
+write_csv(data4, paste0(folder_path, "NINFAS-2023-limpiado.csv"))
+write_csv(data5, paste0(folder_path, "NINFAS-2024-limpiado.csv"))
 
 # Listar todos los archivos CSV en la carpeta
 file_list <- list.files(path = folder_path, pattern = "*.csv", full.names = TRUE)
 
-# Leer y concatenar todos los archivos CSV en un solo DataFrame
-all_data <- lapply(file_list, read_csv) %>% bind_rows()
+# Leer y concatenar todos los archivos CSV, tomando solo las primeras 8 columnas
+all_data <- lapply(file_list, function(file) {
+  read_csv(file, col_names = TRUE) %>% select(1:8)
+}) %>% bind_rows()
 
 
 
@@ -112,3 +139,64 @@ write_csv(all_data, paste0(folder_path, "NINFAS_2020-2024_combinado.csv"))
 
 # Mostrar las primeras filas del DataFrame combinado
 head(all_data)
+
+na_count <- colSums(is.na(all_data))
+na_count
+
+# Borrar las filas que tienen valores NA
+all_data <- all_data %>% drop_na()
+
+na_count
+
+# -----------------------------------------
+# UTP
+library(dplyr)
+library(readr)
+library(tidyr)
+
+data1 <- read_csv("Desktop/UNIVERSITY/Servicio-Social/Data-Mining/Datos/UTP/limpios/UTP-2020-limpiado.csv")
+data2 <- read_csv("Desktop/UNIVERSITY/Servicio-Social/Data-Mining/Datos/UTP/limpios/UTP-2021-limpiado.csv")
+data3 <- read_csv("Desktop/UNIVERSITY/Servicio-Social/Data-Mining/Datos/UTP/limpios/UTP-2022-limpiado.csv")
+data4 <- read_csv("Desktop/UNIVERSITY/Servicio-Social/Data-Mining/Datos/UTP/limpios/UTP-2023-limpiado.csv")
+data5 <- read_csv("Desktop/UNIVERSITY/Servicio-Social/Data-Mining/Datos/UTP/limpios/UTP-2024-limpiado.csv")
+
+
+colnames(data1) <- c("FECHA", "Horas", "O3", "NO2", "CO", "SO2", "PM-10", "PM-2.5")
+colnames(data2) <- c("FECHA", "Horas", "O3", "NO2", "CO", "SO2", "PM-10", "PM-2.5")
+colnames(data3) <- c("FECHA", "Horas", "O3", "NO2", "CO", "SO2", "PM-10", "PM-2.5")
+colnames(data4) <- c("FECHA", "Horas", "O3", "NO2", "CO", "SO2", "PM-10", "PM-2.5")
+colnames(data5) <- c("FECHA", "Horas", "O3", "NO2", "CO", "SO2", "PM-10", "PM-2.5")
+
+# Definir la ruta a la carpeta que contiene los archivos CSV
+folder_path <- "~/Desktop/UNIVERSITY/Servicio-Social/Data-Mining/Datos/UTP/limpios/"
+
+# Exportar el DataFrame combinado a un nuevo archivo CSV
+write_csv(data1, paste0(folder_path, "UTP-2020-limpiado.csv"))
+write_csv(data2, paste0(folder_path, "UTP-2021-limpiado.csv"))
+write_csv(data3, paste0(folder_path, "UTP-2022-limpiado.csv"))
+write_csv(data4, paste0(folder_path, "UTP-2023-limpiado.csv"))
+write_csv(data5, paste0(folder_path, "UTP-2024-limpiado.csv"))
+
+# Listar todos los archivos CSV en la carpeta
+file_list <- list.files(path = folder_path, pattern = "*.csv", full.names = TRUE)
+
+# Leer y concatenar todos los archivos CSV, tomando solo las primeras 8 columnas
+all_data <- lapply(file_list, function(file) {
+  read_csv(file, col_names = TRUE) %>% select(1:8)
+}) %>% bind_rows()
+
+
+
+# Exportar el DataFrame combinado a un nuevo archivo CSV
+write_csv(all_data, paste0(folder_path, "UTP_2020-2024_combinado.csv"))
+
+# Mostrar las primeras filas del DataFrame combinado
+head(all_data)
+
+na_count <- colSums(is.na(all_data))
+na_count
+
+# Borrar las filas que tienen valores NA
+all_data <- all_data %>% drop_na()
+
+na_count
