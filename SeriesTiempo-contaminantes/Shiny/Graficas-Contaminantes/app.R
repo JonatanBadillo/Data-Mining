bc2bb9a4848f7da0f4ab22f91c814155b2a9f854
Jonatan Badillo <jonatanbadillo.19@gmail.com>
2024-08-24 19:23:51 -0600

    ARIMA PT 1 HORAS

--- SeriesTiempo-contaminantes/Shiny/Graficas-Contaminantes/app.R
+++ SeriesTiempo-contaminantes/Shiny/Graficas-Contaminantes/app.R
@@ -1,71 +1,64 @@
-library(shiny)      # Para construir la aplicación Shiny
-library(ggplot2)    # Para la visualización de datos
-library(dplyr)      # Para la manipulación de datos
-library(lubridate)  # Para el manejo de fechas y horas
-library(tidyr)      # Para la transformación de datos
-library(readr)      # Para la lectura de archivos CSV
+library(shiny)
+library(ggplot2)
+library(dplyr)
+library(lubridate)
+library(tidyr)
+library(readr)
+library(forecast)  # Librería para predicciones con ARIMA
 
 # Define la función para cargar y procesar datos
 load_and_process_data <- function(station, year, type) {
-  # Reemplaza "AGUA SANTA" por "AGUASANTA" en el nombre de la estación
   station <- gsub("AGUA SANTA", "AGUASANTA", station)
   
   if (type == "year") {
-    # Para el gráfico anual, leer datos de todos los años
     years <- c("2020", "2021", "2022", "2023", "2024")
     data_list <- lapply(years, function(y) {
       file_path <- paste0("~/Desktop/UNIVERSITY/Servicio-Social/Data-Mining/SeriesTiempo-contaminantes/datos-limpios/", station, "/", station, "-", y, "-limpiado.csv")
       data <- tryCatch({
         read_csv(file_path, col_types = cols())
       }, error = function(e) {
-        return(NULL)  # Retorna NULL si hay un error al leer el archivo
+        return(NULL)
       })
       if (!is.null(data)) {
         data <- data %>%
-          mutate(Datetime = dmy_hms(paste(FECHA, Horas))) %>%  # Crear columna Datetime combinando FECHA y Horas
-          select(-FECHA, -Horas) %>%  # Eliminar las columnas FECHA y Horas
-          arrange(Datetime) %>%  # Ordenar los datos por Datetime
-          pivot_longer(cols = c(O3, NO2, CO, SO2, `PM-10`, `PM-2.5`), names_to = "Contaminante", values_to = "Concentracion") %>%  # Transformar los datos a formato largo
-          mutate(Year = year(Datetime)) %>%  # Extraer el año de la columna Datetime
-          select(-Datetime)  # Eliminar la columna Datetime
+          mutate(Datetime = dmy_hms(paste(FECHA, Horas))) %>%
+          select(-FECHA, -Horas) %>%
+          arrange(Datetime) %>%
+          pivot_longer(cols = c(O3, NO2, CO, SO2, `PM-10`, `PM-2.5`), names_to = "Contaminante", values_to = "Concentracion") %>%
+          mutate(Year = year(Datetime)) %>%
+          select(-Datetime)
         return(data)
       } else {
         return(NULL)
       }
     })
-    # Combinar todos los datos en un solo data frame y calcular el promedio de concentración por año y contaminante
     data <- bind_rows(data_list) %>%
       group_by(Year, Contaminante) %>%
       summarise(Promedio_Concentracion = mean(Concentracion, na.rm = TRUE), .groups = 'drop')
   } else {
-    # Para los gráficos por hora y por mes, leer datos de un solo año
     file_path <- paste0("~/Desktop/UNIVERSITY/Servicio-Social/Data-Mining/SeriesTiempo-contaminantes/datos-limpios/", station, "/", station, "-", year, "-limpiado.csv")
     data <- tryCatch({
       read_csv(file_path, col_types = cols())
     }, error = function(e) {
-      return(NULL)  # Retorna NULL si hay un error al leer el archivo
+      return(NULL)
     })
-    
     if (is.null(data)) {
       return(NULL)
     }
-    
     data <- data %>%
-      mutate(Datetime = dmy_hms(paste(FECHA, Horas))) %>%  # Crear columna Datetime combinando FECHA y Horas
-      select(-FECHA, -Horas) %>%  # Eliminar las columnas FECHA y Horas
-      arrange(Datetime) %>%  # Ordenar los datos por Datetime
+      mutate(Datetime = dmy_hms(paste(FECHA, Horas))) %>%
+      select(-FECHA, -Horas) %>%
+      arrange(Datetime) %>%
       pivot_longer(cols = c(O3, NO2, CO, SO2, `PM-10`, `PM-2.5`), names_to = "Contaminante", values_to = "Concentracion")
     
     if (type == "hour") {
-      # Para gráficos por hora: calcular el promedio de concentración por hora y contaminante
       data <- data %>%
-        mutate(Hora = hour(Datetime)) %>%  # Extraer la hora de la columna Datetime
+        mutate(Hora = hour(Datetime)) %>%
         group_by(Hora, Contaminante) %>%
         summarise(Promedio_Concentracion = mean(Concentracion, na.rm = TRUE), .groups = 'drop')
     } else if (type == "month") {
-      # Para gráficos por mes: calcular el promedio de concentración por mes y contaminante
       data <- data %>%
-        mutate(Mes = month(Datetime, label = TRUE, abbr = TRUE)) %>%  # Extraer el mes de la columna Datetime
+        mutate(Mes = month(Datetime, label = TRUE, abbr = TRUE)) %>%
         group_by(Mes, Contaminante) %>%
         summarise(Promedio_Concentracion = mean(Concentracion, na.rm = TRUE), .groups = 'drop')
     }
@@ -81,13 +74,16 @@ ui <- fluidPage(
     sidebarPanel(
       selectInput("graphType", "Seleccionar Tipo de Gráfico:",
                   choices = c("Por Hora" = "hour", "Por Año" = "year", "Por Mes" = "month")),
-      
-      uiOutput("station_ui"),  # Salida dinámica para seleccionar la estación
-      uiOutput("time_ui"),     # Salida dinámica para seleccionar el año (solo para gráficos que no son por año)
-      uiOutput("contaminants_ui")  # Salida dinámica para seleccionar los contaminantes
+      uiOutput("station_ui"),
+      uiOutput("time_ui"),
+      uiOutput("contaminants_ui"),
+      conditionalPanel(
+        condition = "input.graphType == 'hour'",
+        checkboxInput("showPrediction", "Mostrar Predicción ARIMA", value = FALSE)
+      )
     ),
     mainPanel(
-      plotOutput("contaminantPlot")  # Salida dinámica para el gráfico
+      plotOutput("contaminantPlot")
     )
   )
 )
@@ -95,7 +91,6 @@ ui <- fluidPage(
 # Define la función del servidor
 server <- function(input, output, session) {
   
-  # Reacciona a los cambios en el tipo de gráfico para actualizar los controles de entrada
   observe({
     if (input$graphType == "year") {
       output$station_ui <- renderUI({
@@ -103,7 +98,7 @@ server <- function(input, output, session) {
                     choices = c("NINFAS", "UTP", "AGUA SANTA", "BINE"))
       })
       output$time_ui <- renderUI({
-        NULL  # Elimina la selección de año para gráficos por año
+        NULL
       })
       output$contaminants_ui <- renderUI({
         selectInput("contaminants", "Seleccionar Contaminantes:",
@@ -125,12 +120,8 @@ server <- function(input, output, session) {
     }
   })
   
-  # Reacciona a los cambios en el tipo de gráfico y en los filtros para cargar datos y generar el gráfico
   output$contaminantPlot <- renderPlot({
     data <- load_and_process_data(input$station, input$year, input$graphType)
-    
-    # Depuración: Imprimir los primeros registros del dataset cargado
-    print(head(data))
     
     if (input$graphType == "hour") {
       if (is.null(data)) {
@@ -138,20 +129,34 @@ server <- function(input, output, session) {
           labs(title = paste("Error: No se encontraron suficientes datos para", input$station, input$year), x = NULL, y = NULL) +
           theme_void()
       } else {
-        if ("ALL" %in% input$contaminants) {
-          filtered_data <- data
-        } else {
-          filtered_data <- data %>%
-            filter(Contaminante %in% input$contaminants)
-        }
+        filtered_data <- if ("ALL" %in% input$contaminants) data else data %>% filter(Contaminante %in% input$contaminants)
         
-        ggplot(filtered_data, aes(x = Hora, y = Promedio_Concentracion, color = Contaminante, group = Contaminante)) +
+        p <- ggplot(filtered_data, aes(x = Hora, y = Promedio_Concentracion, color = Contaminante, group = Contaminante)) +
           geom_line(size = 1) +
           facet_wrap(~ Contaminante, scales = "free_y") +
           labs(title = paste("Promedio de concentración de contaminantes por hora en Puebla -", input$station, input$year),
                x = "Hora del día",
                y = "Promedio de concentración") +
           theme_minimal()
+        
+        if (input$showPrediction) {
+          predicciones <- lapply(unique(filtered_data$Contaminante), function(contaminante) {
+            datos_contaminante <- filtered_data %>% filter(Contaminante == contaminante)
+            ts_data <- ts(datos_contaminante$Promedio_Concentracion, frequency = 24)
+            fit <- auto.arima(ts_data)
+            pred <- forecast(fit, h = 24)
+            pred_df <- data.frame(Hora = (max(datos_contaminante$Hora) + 1):(max(datos_contaminante$Hora) + 24),
+                                  Prediccion = pred$mean,
+                                  Contaminante = contaminante)
+            return(pred_df)
+          })
+          
+          predicciones <- bind_rows(predicciones)
+          
+          p <- p + geom_line(data = predicciones, aes(x = Hora, y = Prediccion, color = Contaminante, linetype = "Predicción"), size = 1, inherit.aes = FALSE)
+        }
+        
+        p
       }
     } else if (input$graphType == "month") {
       if (is.null(data)) {
@@ -159,12 +164,7 @@ server <- function(input, output, session) {
           labs(title = paste("Error: No se encontraron suficientes datos para", input$station, input$year), x = NULL, y = NULL) +
           theme_void()
       } else {
-        if ("ALL" %in% input$contaminants) {
-          filtered_data <- data
-        } else {
-          filtered_data <- data %>%
-            filter(Contaminante %in% input$contaminants)
-        }
+        filtered_data <- if ("ALL" %in% input$contaminants) data else data %>% filter(Contaminante %in% input$contaminants)
         
         ggplot(filtered_data, aes(x = Mes, y = Promedio_Concentracion, color = Contaminante, group = Contaminante)) +
           geom_line(size = 1) +
@@ -180,12 +180,7 @@ server <- function(input, output, session) {
           labs(title = paste("Error: No se encontraron suficientes datos para", input$station), x = NULL, y = NULL) +
           theme_void()
       } else {
-        if ("ALL" %in% input$contaminants) {
-          filtered_data <- data
-        } else {
-          filtered_data <- data %>%
-            filter(Contaminante %in% input$contaminants)
-        }
+        filtered_data <- if ("ALL" %in% input$contaminants) data else data %>% filter(Contaminante %in% input$contaminants)
         
         ggplot(filtered_data, aes(x = Year, y = Promedio_Concentracion, color = Contaminante, group = Contaminante)) +
           geom_line(size = 1) +
